a195e3ad173946c91a8896887870ee337a9eb041
Renan Paredes Barreto <Renan.barretoo@hotmail.com>
2020-11-09 16:48:48 -0300

    update Editor.js - image upload
    Fixes #7

--- frontend/src/pages/Home/Editor.js
+++ frontend/src/pages/Home/Editor.js
@@ -13,6 +13,28 @@ export default function TextEditor() {
         height: 500,
         menubar: true,
         paste_data_images: true,
+        image_title: true,
+        automatic_uploads: true,
+        file_picker_types: 'image',
+        file_picker_callback: function (cb, value, meta){
+          var input = document.createElement('input');
+          input.setAttribute('type', 'file');
+          input.setAttribute('accept', 'image/*');
+          input.onchange = function (){
+            var file = this.files[0];
+            var reader = new FileReader();
+            reader.onload = function() {
+              var id = 'blobid' + (new Date()).getTime();
+              var blobCache =  window.tinymce.activeEditor.editorUpload.blobCache;
+              var base64 = reader.result.split(',')[1];
+              var blobInfo = blobCache.create(id, file, base64);
+              blobCache.add(blobInfo);
+              cb(blobInfo.blobUri(), { title: file.name });
+            };
+            reader.readAsDataURL(file);
+          };
+          input.click();
+        },
         plugins: [
           'advlist autolink lists link image charmap print preview anchor',
           'searchreplace visualblocks code fullscreen',
