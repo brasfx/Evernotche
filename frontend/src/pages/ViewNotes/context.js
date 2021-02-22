4ee2fcd29928ac49154e24258868df4d630a1217
Renan Paredes Barreto <Renan.barretoo@hotmail.com>
2021-02-22 17:34:22 -0300

    Update context.js

--- frontend/src/pages/ViewNotes/context.js
+++ frontend/src/pages/ViewNotes/context.js
@@ -6,18 +6,15 @@ const user = {
 };
 console.log('user ' + user.userid);
 
-
-const getNotes = routesServices.findNote(user);
-
 const aux = [];
-
-getNotes.then(function(result) {
+routesServices.findNote(user).then(function(result) {
     result.data.map((entry, i) => {
         aux.push({id: entry._id, content: entry.payload, owner: entry.userid, timestamp: entry.timestamp, selected: false})
     })
     //console.log(JSON.stringify(notesAux));
     return aux;
  });
+
 
 const NotesContext = React.createContext({
     currentNode: null,
