a9217bb73bd8abaf6b54f0dee71a5680fb9d2fa6
davi.ribeiro <foxpc18@gmail.com>
2021-04-08 16:27:42 -0300

    insert button edit note visualization fixes #35

--- frontend/src/pages/ShowNote/index.js
+++ frontend/src/pages/ShowNote/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import './style.css';
 
-export default function ShowNote() {
+export default function ShowNote({ note, dispatch }) {
   const { state } = useLocation();
 
   return (
@@ -10,11 +10,24 @@ export default function ShowNote() {
       <section className="text">
         <div dangerouslySetInnerHTML={{ __html: state.notes.content }} />
       </section>
-      <Link to="/viewnotes">
-        <button className="btn waves-effect waves-light btn-small ">
-          Voltar
-        </button>
-      </Link>
+
+      <section className="buttons">
+        <Link
+          to={{
+            pathname: '/editnote/' + state.notes.id,
+          }}
+        >
+          <button className="edit, waves-effect waves-light btn-small green darken-2 ">
+            Editar
+          </button>
+        </Link>
+
+        <Link to="/viewnotes">
+          <button className="btn waves-effect waves-light btn-small ">
+            Voltar
+          </button>
+        </Link>
+      </section>
     </div>
   );
 }
