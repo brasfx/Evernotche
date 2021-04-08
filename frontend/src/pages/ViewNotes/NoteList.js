1db14f7958d6237a662248eb6b8c7d9b4a34784b
BrunoMDV <72626913+BrunoMDV@users.noreply.github.com>
2021-04-08 20:06:15 -0300

    grid

--- frontend/src/pages/ViewNotes/NoteList.js
+++ frontend/src/pages/ViewNotes/NoteList.js
@@ -1,16 +1,24 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import Note from './Note.js';
 import routesServices from '../../services/routesServices';
 import './style.css';
 
-export default function NoteList({ notes, dispatch }) {
+export default function NoteList({ notes, dispatch, rows }) {
+
+
+
+
+
   return (
-    <div className="notes-container">
-      {Object.keys(notes).map((id, i) => {
-        const note = notes[id];
-        return <Note note={note} dispatch={dispatch} key={i} />;
-      })}
-    </div>
+    <div className="notes-container" style={{ gridTemplateColumns: `${rows ? "700px" : "600px 600px 600px"}` }}>
+
+      {
+        Object.keys(notes).map((id, i) => {
+          const note = notes[id];
+          return <Note note={note} dispatch={dispatch} key={i} />;
+        })
+      }
+    </div >
   );
 }
