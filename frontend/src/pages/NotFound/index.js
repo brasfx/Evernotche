25cd118fa75a7caa9db8e03967c7df3b2934048d
davi.ribeiro <foxpc18@gmail.com>
2021-04-08 14:06:48 -0300

    insert icon

--- frontend/src/pages/NotFound/index.js
+++ frontend/src/pages/NotFound/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function NotFound() {
   return (
     <div>
-      <h1 style={{ color: 'red' }}>Error 404 - Page Not Found </h1>
+      <h3 style={{ color: 'red' }}>Error 404 - Page Not Found </h3>
     </div>
   );
 }
