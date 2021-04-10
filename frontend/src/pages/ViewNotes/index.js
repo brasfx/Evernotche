08e2e50e49aae1e76f9fa846c80de04ab63e738b
FelpsZMS <souzamarquesfelipe@gmail.com>
2021-04-10 02:49:30 -0300

    Search bar feature

--- frontend/src/pages/ViewNotes/index.js
+++ frontend/src/pages/ViewNotes/index.js
@@ -1,46 +1,64 @@
-import React, { useContext, useReducer, useEffect, useState } from 'react';
-import Navbar from '../Home/Navbar.js';
-import notesReducer from './reducer.js';
-import NoteList from './NoteList.js';
-import Panel from './Panel.js';
-import routesServices from '../../services/routesServices'
-
-
+import React, { useContext, useReducer, useEffect, useState } from "react";
+import Navbar from "../Home/Navbar.js";
+import notesReducer from "./reducer.js";
+import NoteList from "./NoteList.js";
+import Panel from "./Panel.js";
+import routesServices from "../../services/routesServices";
 
 export default function ViewNotes() {
-
   const initialState = {};
   const [state, dispatch] = useReducer(notesReducer, initialState);
   const [rows, setRows] = useState(false);
+  const [searchQuery, setSearchQuery] = useState();
 
   function cngRows() {
-    setRows(!rows)
-
+    setRows(!rows);
   }
   useEffect(() => {
     const user = {
-      userid: localStorage.getItem('id')
+      userid: localStorage.getItem("id"),
     };
+
     routesServices.findNote(user).then(function (result) {
-      console.log(result);
-      const notesData = result.data.reduce((acc, entry) => {
+      let filteredResult = result.data;
+
+      if (searchQuery) {
+        const upperCaseSearchQuery = searchQuery.toUpperCase();
+
+        filteredResult = filteredResult.filter(
+          (e) =>
+            e.title.toUpperCase().includes(upperCaseSearchQuery) ||
+            e.payload.toUpperCase().includes(upperCaseSearchQuery)
+        );
+      }
+
+      const notesData = filteredResult.reduce((acc, entry) => {
         const { _id, payload, userid, timestamp, title } = entry;
-        acc[_id] = { id: _id, title, content: payload, owner: userid, timestamp, selected: false };
+        acc[_id] = {
+          id: _id,
+          title,
+          content: payload,
+          owner: userid,
+          timestamp,
+          selected: false,
+        };
         return acc;
-      }, {})
+      }, {});
 
       dispatch({ data: notesData, type: "UPDATE" });
     });
-  }, []);
-
-
+  }, [searchQuery]);
 
   return (
     <div>
       <Navbar />
-      <Panel notes={state} dispatch={dispatch} cngRows={cngRows} />
+      <Panel
+        notes={state}
+        dispatch={dispatch}
+        cngRows={cngRows}
+        setSearchQuery={setSearchQuery}
+      />
       <NoteList notes={state} dispatch={dispatch} rows={rows} />
     </div>
-
   );
 }
